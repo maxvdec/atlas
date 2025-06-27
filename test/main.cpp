e9265ea03bb60ee26deb1fbcb1eb8b386538a0ca
Max Van den Eynde <maxvdec@gmail.com>
2025-06-27 12:51:57 +0200

    setup done

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+/*
+ main.cpp
+ As part of the Atlas project
+ Created by Max Van den Eynde in 2025
+ --------------------------------------------------
+ Description: The main entry point for the Atlas Test
+ Copyright (c) 2025 maxvdec
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
