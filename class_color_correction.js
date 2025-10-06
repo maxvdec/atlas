2836afb0588e164ce1276ff6f31b2ed6a3f969cc
maxvdec <maxvdec@users.noreply.github.com>
2025-10-06 14:19:51 +0000

    deploy: ebcef9e7810e9d26bef9ce42859e10231fba9dd7

--- class_color_correction.js
+++ class_color_correction.js
@@ -2,6 +2,6 @@ var class_color_correction =
 [
     [ "ColorCorrection", "class_color_correction.html#a6db3892398a3dfc92a8629c48b7d661e", null ],
     [ "applyToProgram", "class_color_correction.html#a4f3ee4341b7750d947d5f7ab7ead32c9", null ],
-    [ "create", "class_color_correction.html#aaea52b96e771fc763274fb47d73a2222", null ],
+    [ "create", "class_color_correction.html#a6191c102b6696b537e6a29e8acdf23f5", null ],
     [ "params", "class_color_correction.html#ae7dedd2d2f9b3cd1e310d2a9d8525b35", null ]
 ];
